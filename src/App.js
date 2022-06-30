4615cb8814e0e4eafad0428cbe0d7a565b7b1c9b
christierbt <98954266+christierbt@users.noreply.github.com>
2022-06-30 18:22:55 +0800

    added structure and css

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h1>Weather App</h1>
-        <Weather />
-        <footer>
-          <a
-            href="https://github.com/christierbt/christie-react-weatherapp"
-            target="_blank"
-          >
-            Open Source Code
-          </a>{" "}
-          by Christie Rosenblatt
-        </footer>
-      </div>
+      <Weather />
+      <footer>
+        <a
+          href="https://github.com/christierbt/christie-react-weatherapp"
+          target="_blank"
+        >
+          Open Source Code
+        </a>{" "}
+        by Christie Rosenblatt
+      </footer>
     </div>
   );
 }
