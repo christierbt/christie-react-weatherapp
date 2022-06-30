b79d84b1676f71eb3c40c1d3d0a537f516d6bdf6
christierbt <98954266+christierbt@users.noreply.github.com>
2022-06-30 18:01:43 +0800

    added bootstrap css

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <p>
-        <a
-          href="https://github.com/christierbt/christie-react-weatherapp"
-          target="_blank"
-        >
-          Open Source Code
-        </a>{" "}
-        by Christie Rosenblatt
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/christierbt/christie-react-weatherapp"
+            target="_blank"
+          >
+            Open Source Code
+          </a>{" "}
+          by Christie Rosenblatt
+        </footer>
+      </div>
     </div>
   );
 }
