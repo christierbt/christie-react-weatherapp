044a1e85df69f157b1877458ea076f31c25d62f8
christierbt <98954266+christierbt@users.noreply.github.com>
2022-06-30 18:07:27 +0800

    created weather.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           <a
             href="https://github.com/christierbt/christie-react-weatherapp"
