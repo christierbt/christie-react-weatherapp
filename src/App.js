280275f6be0ef885d8f7efdccb3d3ebe53d420fb
christierbt <98954266+christierbt@users.noreply.github.com>
2022-07-05 13:44:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ function App() {
   return (
     <div className="App">
       <Weather />
-      <footer>
-        <a
-          href="https://github.com/christierbt/christie-react-weatherapp"
-          target="_blank"
-        >
-          Open Source Code
-        </a>{" "}
-        by Christie Rosenblatt
-      </footer>
     </div>
   );
 }
