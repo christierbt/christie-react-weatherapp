0288c5347cf2a07adc68292943354dc5aa97fbaf
christierbt <98954266+christierbt@users.noreply.github.com>
2022-07-06 13:58:18 +0800

    Added date API integration

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather />
-      <footer>
-        <a
-          rel="noopener noreferrer"
-          href="https://github.com/christierbt/christie-react-weatherapp"
-          target="_blank"
-        >
-          Open Source Code
-        </a>{" "}
-        by Christie Rosenblatt
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          <a
+            rel="noopener noreferrer"
+            href="https://github.com/christierbt/christie-react-weatherapp"
+            target="_blank"
+          >
+            Open Source Code
+          </a>{" "}
+          by Christie Rosenblatt
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
+import FormattedDate from "./FormattedDate";
 
 export default function Weather() {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -8,7 +9,7 @@ export default function Weather() {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
-      date: "Wednesday 6th July, 2:00pm",
+      date: new Date(response.data.dt * 1000),
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       city: response.data.name,
@@ -20,77 +21,75 @@ export default function Weather() {
 
   if (weatherData.ready) {
     return (
-      <div className="container">
-        <form id="search-form">
-          <div className="row">
-            <div className="col-6">
-              <input
-                className="enter-city w-100"
-                type="city"
-                placeholder="Enter your city..."
-                id="search-city-input"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                className="btn btn-primary w-100"
-                type="submit"
-                value="Search"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                className="btn btn-primary w-100"
-                id="current-button"
-                type="submit"
-                value="Current"
-              />
-            </div>
+      <form id="search-form">
+        <div className="row">
+          <div className="col-6">
+            <input
+              className="enter-city w-100"
+              type="city"
+              placeholder="Enter your city..."
+              id="search-city-input"
+            />
           </div>
-          <div className="col-9">
-            <h1 id="city">{weatherData.city}</h1>
+          <div className="col-3">
+            <input
+              className="btn btn-primary w-100"
+              type="submit"
+              value="Search"
+            />
           </div>
-          <h2>
-            Last updated: {weatherData.date}
-            <span id="current-date"></span>
-          </h2>
+          <div className="col-3">
+            <input
+              className="btn btn-primary w-100"
+              id="current-button"
+              type="submit"
+              value="Current"
+            />
+          </div>
+        </div>
+        <div className="col-9">
+          <h1 id="city">{weatherData.city}</h1>
+        </div>
+        <h2>
+          Last updated: <FormattedDate date={weatherData.date} />
+          <span id="current-date"></span>
+        </h2>
 
-          <p id="description" className="text-capitalize">
-            {weatherData.description}
-          </p>
-          <div className="row">
-            <div className="col-3">
-              <img
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                id="weather-icon"
-                className="float-left"
-              />
-            </div>
-
-            <div className="col-3">
-              <div className="current-temperature">
-                <span id="current-temp">
-                  {Math.round(weatherData.temperature)}
-                </span>
-                <span className="units">°C</span>
-              </div>
-            </div>
-            <div className="col-4 weather-details">
-              <ul>
-                <li>
-                  Wind: <span id="wind">{Math.round(weatherData.wind)}</span>
-                  km/h
-                </li>
-                <li>
-                  Humidity: <span id="humidity">{weatherData.humidity}</span>%
-                </li>
-              </ul>
-            </div>
-            <div className="weather-forecast" id="forecast"></div>
+        <p id="description" className="text-capitalize">
+          {weatherData.description}
+        </p>
+        <div className="row">
+          <div className="col-3">
+            <img
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
+              id="weather-icon"
+              className="float-left"
+            />
           </div>
-        </form>
-      </div>
+
+          <div className="col-3">
+            <div className="current-temperature">
+              <span id="current-temp">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="units">°C</span>
+            </div>
+          </div>
+          <div className="col-4 weather-details">
+            <ul>
+              <li>
+                Wind: <span id="wind">{Math.round(weatherData.wind)}</span>
+                km/h
+              </li>
+              <li>
+                Humidity: <span id="humidity">{weatherData.humidity}</span>%
+              </li>
+            </ul>
+          </div>
+          <div className="weather-forecast" id="forecast"></div>
+        </div>
+      </form>
     );
   } else {
     const apiKey = "4b433d9a7f22e9aa26d3e1760e73b0a6";
