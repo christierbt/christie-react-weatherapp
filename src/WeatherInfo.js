e47db173cefda37dc777357d27845e42af9c6b56
christierbt <98954266+christierbt@users.noreply.github.com>
2022-07-08 17:02:08 +0800

    Added animated icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,7 +19,7 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-3">
           <div className="float-left">
-            <WeatherIcon code={props.data.icon} alt={props.data.description} />
+            <WeatherIcon code={props.data.icon} size={50} />
           </div>
         </div>
 
