25c97639d4fefc565712ef301b5f485b549a155d
christierbt <98954266+christierbt@users.noreply.github.com>
2022-07-08 17:14:09 +0800

    changes to icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,7 +19,7 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-3">
           <div className="float-left">
-            <WeatherIcon code={props.data.icon} size={50} />
+            <WeatherIcon code={props.data.icon} size={48} />
           </div>
         </div>
 
